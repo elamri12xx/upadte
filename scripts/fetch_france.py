b3624a5d25a021c76c350a2b1a0add80197119d8
elamri12xx <84471797+elamri12xx@users.noreply.github.com>
2025-07-11 21:16:46 +0100

    Update fetch_france.py

--- scripts/fetch_france.py
+++ scripts/fetch_france.py
@@ -1,1 +1,7 @@
+import requests
 
+url = "https://api-v2.fallakatv.eu.org/france.php"
+response = requests.get(url)
+
+with open("france.json", "w", encoding="utf-8") as f:
+    f.write(response.text)
